d7d3d5643d244aaadff5a106fcdb35e0438cf592
diegoasencios2010 <82003210+diegoasencios2010@users.noreply.github.com>
2021-04-15 06:01:20 -0500

    Add files via upload

---
+++ trabajo EC2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+nombre = "diego"
+print("Hola, ya terminé el EC2. Saludos. " + nombre)
+
