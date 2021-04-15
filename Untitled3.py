6c68476c811743583a4f055e2f8823ce02247b65
diegoasencios2010 <82003210+diegoasencios2010@users.noreply.github.com>
2021-04-15 06:31:19 -0500

    ejercicio corto 2

---
+++ Untitled3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+nombre = "diego asencios ore"
+print("Hola, ya terminé el EC2. Saludos. " + nombre)
+
